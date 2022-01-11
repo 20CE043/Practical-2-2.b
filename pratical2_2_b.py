05e34808ece41e785ad5767175390dab70a18d65
20CE043 <97497245+20CE043@users.noreply.github.com>
2022-01-11 10:57:04 +0530

    Add files via upload

---
+++ pratical2_2_b.py
@@ -0,0 +1,8 @@
+#Name: Hena Kharwa
+#ID: 20CE043
+#Github link: https://github.com/20CE043/Practical-2-2.b.git
+#Aim: Write a Python program to create a tuple with numbers and print one item.
+tuple=(1,2,3,4,5)
+i=int(input("Enter index: "))
+#accessing data value on index i from the tuple
+print(tuple[i])
